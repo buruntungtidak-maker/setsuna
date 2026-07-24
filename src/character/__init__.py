011b13c116215118274908e2afb0d6c130c8e123
buruntungtidak-maker <buruntungtidak@gmail.com>
2026-07-24 21:15:45 +0800

    fix: export Emotion and CharacterState from character module

--- src/character/__init__.py
+++ src/character/__init__.py
@@ -4,6 +4,6 @@ Character module: Character rendering and management
 
 from .character_part import CharacterPart, PartType
 from .character_sprite import CharacterSprite
-from .character_manager import CharacterManager
+from .character_manager import CharacterManager, Emotion, CharacterState
 
-__all__ = ["CharacterPart", "PartType", "CharacterSprite", "CharacterManager"]
+__all__ = ["CharacterPart", "PartType", "CharacterSprite", "CharacterManager", "Emotion", "CharacterState"]
