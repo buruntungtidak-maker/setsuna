9e6cb31ddb7fba7f3c0dabf1083a132408c82ee7
setsu9721-ai <setsu9721@gmail.com>
2026-07-25 08:23:06 +0800

    Initialize character module with foundation classes

--- src/character/__init__.py
+++ src/character/__init__.py
@@ -1,5 +1,19 @@
 """
-Character module: Character rendering and management
+Character module: Character rendering, management, and composition
 """
 
-__all__ = []
+from .character_scanner import CharacterScanner
+from .asset_database import AssetDatabase
+from .asset_manager import AssetManager
+from .character_loader import CharacterLoader
+from .layer_renderer import LayerRenderer
+from .expression_controller import ExpressionController
+
+__all__ = [
+    'CharacterScanner',
+    'AssetDatabase',
+    'AssetManager',
+    'CharacterLoader',
+    'LayerRenderer',
+    'ExpressionController',
+]
